e572d9f7d7ffc567ea469d0751aac1d493579048
Karen Yip <karenyip1999@gmail.com>
2024-02-22 16:17:19 +0000

    Created controller with GET request

---
+++ src/main/java/com/example/task_01/controllers/GreetingController.java
@@ -0,0 +1,20 @@
+package com.example.task_01.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public String greeting()
+    {
+        return "Good afternoon Karen!";
+    }
+
+
+
+
+}
