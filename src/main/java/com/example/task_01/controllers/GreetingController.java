d6eb1e13ba8dccb6a2d2fba09e41d4118e16a1ec
Karen Yip <karenyip1999@gmail.com>
2024-02-28 17:38:42 +0000

    Added .md

--- src/main/java/com/example/task_01/controllers/GreetingController.java
+++ src/main/java/com/example/task_01/controllers/GreetingController.java
@@ -11,6 +11,8 @@ public class GreetingController {
 
     @GetMapping
     public Greeting greeting()
+    //Endpoint http://localhost:8080/greeting?first_name=jack&age=42
+    //public Greeting greet(@RequestParam String first_name, @RequestParam int age)
     {
         Greeting greeting = new Greeting("Karen", "Afternoon");
         return greeting;
